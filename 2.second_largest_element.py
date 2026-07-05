1587d4fdfd9a9a8d392a4a8c40cd6e03e3e6592b
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-07-05 23:16:00 +0530

    print second largest number in  sorted array

--- 2.second_largest_element.py
+++ 2.second_largest_element.py
@@ -1,2 +1,18 @@
 def sec_largest(array):
-    
+    largest = 0
+    sec_largest = 0
+    n = len(array)
+    for i in range(n):
+        if array[i]>largest:
+            sec_largest = largest
+            largest = array[i]
+        
+        elif array[i]>sec_largest and array[i]<largest:
+            sec_largest = array[i]
+
+    return sec_largest, largest
+
+
+array = [1,2,4,7,7,5,8,6]
+
+print(sec_largest(array))

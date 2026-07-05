9e02cddefb1cc7920476307958e97035aae9e811
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-07-05 23:34:04 +0530

    revision

--- 2.second_largest_element.py
+++ 2.second_largest_element.py
@@ -1,18 +1,27 @@
 def sec_largest(array):
-    largest = 0
-    sec_largest = 0
+    sorted(array, reverse=True)
+    # 
+    largest = float("-inf")
+    sec_largest = float("-inf")
     n = len(array)
-    for i in range(n):
-        if array[i]>largest:
-            sec_largest = largest
-            largest = array[i]
-        
-        elif array[i]>sec_largest and array[i]<largest:
-            sec_largest = array[i]
 
-    return sec_largest, largest
+    # if len(array) < 2:
+    #     return -1
+    if not array:
+        return -1
+    else :
+        for i in range(n):
+            if array[i]>largest:
+                sec_largest = largest
+                largest = array[i]
+            
+            elif array[i]>sec_largest and array[i]<largest:
+                sec_largest = array[i]
+
+        return sec_largest, largest
 
 
-array = [1,2,4,7,7,5,8,6]
+# array = [1,2,4,7,7,5,8,6]
+array = []
 
 print(sec_largest(array))

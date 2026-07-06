35ee99088b6c9d3eeb5630fd0f30ddf1fde9d1eb
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-07-07 00:11:32 +0530

    move all zero to end using two pointer

---
+++ 7. move _all_zero_to_end.py
@@ -0,0 +1,14 @@
+def move_zero(array):
+    n = len(array)
+    i = 0
+    for j in range(n):
+        if array[j]!=0: # if it is different
+            array[i], array[j] = array[j], array[i]
+            i+=1
+
+    return array
+
+
+array = [0,0,2,0,18,0,4,0,5,0,6,0]
+
+print(move_zero(array))

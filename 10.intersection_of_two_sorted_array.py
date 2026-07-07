282c9112564cd3f4f128500b81fac3145259ccef
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-07-07 21:42:03 +0530

    intersection of sorted array

--- 10.intersection_of_two_sorted_array.py
+++ 10.intersection_of_two_sorted_array.py
@@ -2,15 +2,20 @@ def intersection(arr1, arr2):
     n = len(arr1)
     m = len(arr2)
     i = 0
+    j = 0
     intersec = []
     
-    for num in arr1:
-        if num in arr2:
-            intersec.append(num)
-            i +=1
+    while i<n and j<m :
+        if arr1[i]<arr2[j] :
+            i+=1
+        elif arr1[i] > arr2[j]:
+            j+=1
+        else :
+            intersec.append(arr1[i])
+            i+=1
+            j+=1
 
-
-    return intersec 
+    return intersec
 
 
 arr1 = [1,3,5,9,10]

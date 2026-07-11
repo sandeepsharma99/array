7502beb7e586123cf4379180290e63519dc56f09
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-07-11 21:31:26 +0530

    majority element

---
+++ 17.majority_element.py
@@ -0,0 +1,19 @@
+# boyer's Moore voting algorith
+def majority_element(array) :
+    count = 0
+    ele = -1
+    for num in array :
+        if count == 0 :
+            ele = num
+        elif ele == num :
+            count += 1
+        else :
+            count -= 1
+        
+    return ele
+
+
+array = [2,2,1,1,1,1,1,1,2,2]
+print(majority_element(array))
+        
+

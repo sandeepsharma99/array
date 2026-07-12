af39848764873247e344bf4456f3de6cf9b76178
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-07-12 19:44:52 +0530

    rearrange pos neg of array ny creating pos neg array and index case if pos is greater than neg or vise versa

--- 20.rearrange_array_ele_by_sign.py
+++ 20.rearrange_array_ele_by_sign.py
@@ -1,32 +1,33 @@
 def rearrange(array) :
-    pos = []
-    neg = []
-    for i in range(len(array)) :
-        if array[i] > 0 :
-            pos.append(array[i])
-        else :
-            neg.append(array[i])
-    
-    if len(pos) > len(neg) : # case 1: if pos ele > neg ele
-        for i in range(len(neg)) :
-            array[2*i] = pos[i]
-            array[2*i+1] = neg[i]
-        
-        j = len(neg)*2
-        for i in range(j,len(array)) :
-            array[i] = pos[j]
-            j+=1
-    else :                          # case 2: if neg ele > pos ele
-        for i in range(len(pos)) :
-            array[2*i] = pos[i]
-            array[2*i+1] = neg[i]
-        
-        j = len(pos)*2
-        for i in range(j,len(array)) :
-            array[i] = neg[j]
-            j+=1
-    return array
+            pos = []
+            neg = []
+            for i in range(len(array)) :
+                if array[i] > 0 :
+                    pos.append(array[i])
+                else :
+                    neg.append(array[i])
+            
+            if len(pos) > len(neg) : # case 1: if pos ele > neg ele
+                for i in range(len(neg)) :
+                    array[2*i] = pos[i]
+                    array[2*i+1] = neg[i]
+                
+                j = len(neg)*2
+                for i in range(len(neg),len(pos)) : #
+                    array[j] = pos[i] # i = 3,4, j = 6,7
+                    j+=1
+            else :                          # case 2: if neg ele > pos ele
+                for i in range(len(pos)) :
+                    array[2*i] = pos[i]
+                    array[2*i+1] = neg[i]
+                
+                j = len(pos)*2
+                for i in range(len(pos),len(neg)) :
+                    array[j] = neg[i]   
+                    j+=1
 
-array = [3,1,-2,-5,2,-4]
+            return array
+
+array = [3,1,-2,-5,2,-4,2,4]
 
 print(rearrange(array))

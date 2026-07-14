de6592509ff5596605c5740dac9151458e61012f
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-07-14 21:15:20 +0530

    implement longest successive elements function

---
+++ 23.longest_successive_array.py
@@ -0,0 +1,31 @@
+def longest_successive_elements(array):
+    n = len(array)
+    if n == 0:
+        return 0
+
+    longest = 1
+
+    # Create a set containing all elements
+    st = set(array)
+
+    # Iterate through each element in the set
+    for num in st:
+
+        # Check if num is the start of a sequence
+        if num - 1 not in st:
+
+            cnt = 1
+            x = num
+
+            # Count consecutive numbers
+            while x + 1 in st:
+                x = x + 1
+                cnt += 1
+
+            longest = max(longest, cnt)
+
+    return longest
+
+
+array = [100, 4, 101, 102, 1, 3, 2]
+print(longest_successive_elements(array))

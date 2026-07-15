927e2b3cff59ac0ad060e8c6a09f6d0b9ebec3dc
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-07-15 22:51:35 +0530

    add comments to clarify transpose and row reversal steps in rotate function

--- 26.rotate_image.py
+++ 26.rotate_image.py
@@ -1,9 +1,10 @@
 def rotate(matrix) :
     n= len(matrix)
-
+    # Transpose
     for i in range(n) :
         for j in range(i,n) :
             matrix[i][j] , matrix[j][i] = matrix[j][i], matrix[i][j]
+    # reverse each row
     for row in matrix :   
         row.reverse()   
     return matrix

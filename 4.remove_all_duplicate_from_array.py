a16789bcf4f44f0261d7101903411de6fb37d968
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-09-06 20:38:29 +0530

    revision

--- 4.remove_all_duplicate_from_array.py
+++ 4.remove_all_duplicate_from_array.py
@@ -1,11 +1,11 @@
 def remove_duplicate(array):
-    array.sort()
+    array.sort()  # modifies original array; it return None
     i = 0
     for j in range(1,len(array)):
         if array[j] != array[i]:  # if it is different
             array[i+1] = array[j] # move curr ele to i+1 
             i+=1  
-    return array[ :i+1]
+    return array[ :i+1] 
 
 array = [1,1,3,3,2,3,2,4,4,4,4]
 
